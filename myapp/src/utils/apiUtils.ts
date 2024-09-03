6268350afeef53a2ac1826b5b53a6fbafba11e3d
seunghyun <tmdgus7048@naver.com>
2024-09-04 04:00:31 +0900

    fix : 삭제

--- myapp/src/utils/apiUtils.ts
+++
@@ -1,7 +0,0 @@
-export const replaceUrlParams = (url: string, params: { [key: string]: string | number }) => {
-  let modifiedUrl = url;
-  Object.keys(params).forEach(key => {
-    modifiedUrl = modifiedUrl.replace(`{${key}}`, params[key].toString());
-  });
-  return modifiedUrl;
-};
