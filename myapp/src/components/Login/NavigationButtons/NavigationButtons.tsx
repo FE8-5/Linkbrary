bbb17fc7a100d37058851d62b1fc61499680e8bc
jaewanLee <wodhks3791@naver.com>
2024-09-04 04:02:23 +0900

    feat : 페이지 이동 버튼들을 담는 컴포넌트 추가

---
+++ myapp/src/components/Login/NavigationButtons/NavigationButtons.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { StyledNavigationButtons } from './NavigationButtonsStyle';
+import LogoButton from '../@Shared/Buttons/LogoButton/LogoButton';
+
+const NavigationButtons: React.FC = () => {
+  return (
+    <div>
+      <StyledNavigationButtons>
+        <LogoButton />
+        <p>회원이 아니신가요?</p>
+      </StyledNavigationButtons>
+    </div>
+  );
+};
+
+export default NavigationButtons;
---
+++ myapp/src/components/Login/NavigationButtons/NavigationButtonsStyle.ts
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+
+// 버튼 스타일 정의
+export const StyledNavigationButtons = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-between;
+  border: none;
+  color: #000000;
+  width: 21.8rem;
+  height: 7.8rem;
+  margin: 0 auto 3rem;
+  font-size: 1.6rem;
+`;
