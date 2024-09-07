b3927bf8b9a18bf742716b0e78e26e89e9f25b16
seunghyun <tmdgus7048@naver.com>
2024-09-07 21:11:10 +0900

    feat : 링크검색 컴포넌트 추가

--- myapp/src/components/Links/SearchLink/SearchLink.tsx
+++ myapp/src/components/Links/SearchLink/SearchLink.tsx
@@ -1,11 +1,35 @@
 import { SearchIcon, SearchInput, SearchLinkContainer } from './SearchLinkStyle';
 import searchIcon from '../../../assets/Icons/search.png';
+import { ChangeEvent, KeyboardEvent, useEffect, useState } from 'react';
+import { SetURLSearchParams } from 'react-router-dom';
 
-const SearchLink = () => {
+interface SearchLinkProps {
+  initSearch: string | null;
+  setSearchParams: SetURLSearchParams;
+}
+
+const SearchLink = ({ initSearch, setSearchParams }: SearchLinkProps) => {
+  const [search, setSearch] = useState<string>(initSearch || '');
+  const handleSearchChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setSearch(e.target.value);
+  };
+  const handleKeyDown = (e: KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === 'Enter') {
+      setSearchParams(search ? { search } : {});
+    }
+  };
+
   return (
     <SearchLinkContainer>
       <SearchIcon src={searchIcon} alt="searchIcon" />
-      <SearchInput type="text" placeholder="링크를 검색해 보세요." />
+      <SearchInput
+        name="search"
+        type="text"
+        placeholder="링크를 검색해 보세요."
+        value={search}
+        onChange={handleSearchChange}
+        onKeyDown={handleKeyDown}
+      />
     </SearchLinkContainer>
   );
 };
