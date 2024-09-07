842ad23cdc46441ae302920aaa5059712b467526
seunghyun <tmdgus7048@naver.com>
2024-09-07 21:13:29 +0900

    feat : linkpage 폴더리스트 컴포넌트 추가

---
+++ myapp/src/components/Links/FolderList/FolderList.tsx
@@ -0,0 +1,35 @@
+import { useState } from 'react';
+import { GetAllFoldersRes } from '../../../types/folderTypes';
+import { FolderButton, FolderListContainer } from './FolderListStyle';
+
+interface FolderListProps {
+  folderList: GetAllFoldersRes[] | undefined;
+  onClick: (folderId: number) => void;
+  selectedFolderId: number | null;
+}
+
+const FolderList = ({ folderList, onClick, selectedFolderId }: FolderListProps) => {
+  const handleClick = (folderId: number) => {
+    onClick(folderId);
+  };
+  return (
+    <FolderListContainer>
+      {folderList && (
+        <>
+          <FolderButton isSelected={selectedFolderId === 0} onClick={() => handleClick(0)}>
+            전체
+          </FolderButton>
+          {folderList.map(folder => {
+            return (
+              <FolderButton isSelected={selectedFolderId === folder.id} onClick={() => handleClick(folder.id)}>
+                {folder.name}
+              </FolderButton>
+            );
+          })}
+        </>
+      )}
+    </FolderListContainer>
+  );
+};
+
+export default FolderList;
