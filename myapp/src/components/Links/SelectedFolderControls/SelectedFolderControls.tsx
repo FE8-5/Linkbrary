5dc7562905623c11106123c0a4ae22ae3a16c08a
seunghyun <tmdgus7048@naver.com>
2024-09-09 18:17:13 +0900

    feat : 선택한폴더이름과 수정삭제번튼이있는 컴포넌트 추가

---
+++ myapp/src/components/Links/SelectedFolderControls/SelectedFolderControls.tsx
@@ -0,0 +1,32 @@
+import { Dispatch, SetStateAction } from 'react';
+import { GetAllFoldersRes } from '../../../types/folderTypes';
+import FolderControls from '../FolderControls/FolderControls';
+import { SelectedFolderControlsContainer, SelectedFolderTitle } from './SelectedFolderControlsStyle';
+import { ItemLinks } from '../../../types/linkTypes';
+
+interface SelectedFolderControlsProps {
+  selectedFolderInfo: GetAllFoldersRes | undefined;
+  setFolderList: Dispatch<SetStateAction<GetAllFoldersRes[] | undefined>>;
+  linkListInfo: ItemLinks;
+}
+
+const SelectedFolderControls = ({ selectedFolderInfo, setFolderList, linkListInfo }: SelectedFolderControlsProps) => {
+  const selectedFolderName = selectedFolderInfo?.name;
+
+  return (
+    <SelectedFolderControlsContainer>
+      <SelectedFolderTitle>{selectedFolderName ? selectedFolderName : '전체'}</SelectedFolderTitle>
+      {selectedFolderInfo && linkListInfo.totalCount ? (
+        <FolderControls
+          selectedFolderInfo={selectedFolderInfo}
+          setFolderList={setFolderList}
+          linkListInfo={linkListInfo}
+        />
+      ) : (
+        ''
+      )}
+    </SelectedFolderControlsContainer>
+  );
+};
+
+export default SelectedFolderControls;
---
+++ myapp/src/components/Links/SelectedFolderControls/SelectedFolderControlsStyle.ts
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+export const SelectedFolderControlsContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  width: 100%;
+`;
+
+export const SelectedFolderTitle = styled.h1`
+  font-size: 2.4rem;
+  font-weight: 600;
+  line-height: 2.8rem;
+`;
