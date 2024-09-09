fd5f96244b7ad2af1f4b09d49e07f37127141884
seunghyun <tmdgus7048@naver.com>
2024-09-09 18:20:29 +0900

    feat : 폴더수정,삭제버튼이있는 컴포넌트 추가

---
+++ myapp/src/components/Links/FolderControls/FolderControls.tsx
@@ -0,0 +1,63 @@
+import { GetAllFoldersRes } from '../../../types/folderTypes';
+import { EditFolder, FolderControlsContainer, FolderControlsImg } from '../FolderControls/FolderControlsStyle';
+import penIcon from '../../../assets/Icons/pen.png';
+import deleteIcon from '../../../assets/Icons/delete.png';
+import DeleteFolderModal from './DeleteFolderModal/DeleteFolderModal';
+import { Dispatch, SetStateAction, useState } from 'react';
+import EditFolderModal from './EditFolderModal/EditFolderModal';
+import { ItemLinks } from '../../../types/linkTypes';
+
+interface FolderControlsProps {
+  selectedFolderInfo: GetAllFoldersRes | undefined;
+  setFolderList: Dispatch<SetStateAction<GetAllFoldersRes[] | undefined>>;
+  linkListInfo: ItemLinks;
+}
+
+const FolderControls = ({ selectedFolderInfo, setFolderList, linkListInfo }: FolderControlsProps) => {
+  const [isDeleteModalOpen, setIsDeleteModalOpen] = useState(false);
+  const [isEditModalOpen, setIsEditModalOpen] = useState(false);
+
+  const closeDeleteModal = () => {
+    setIsDeleteModalOpen(false);
+  };
+  const openDeleteModal = () => {
+    if (!linkListInfo) {
+      setIsDeleteModalOpen(true);
+    } else {
+      alert('폴더가 비어있어야 삭제가 가능합니다.');
+    }
+  };
+  const closeEditModal = () => {
+    setIsEditModalOpen(false);
+  };
+  const openEditModal = () => {
+    setIsEditModalOpen(true);
+  };
+  return (
+    <FolderControlsContainer>
+      <EditFolder onClick={openEditModal}>
+        <FolderControlsImg src={penIcon} alt="penIcon" />
+        <p>이름 변경</p>
+      </EditFolder>
+      <EditFolderModal
+        isModalOpen={isEditModalOpen}
+        closeModal={closeEditModal}
+        folderId={selectedFolderInfo?.id}
+        setFolderList={setFolderList}
+      />
+      <EditFolder onClick={openDeleteModal}>
+        <FolderControlsImg src={deleteIcon} alt="deleteIcon" />
+        <p>삭제</p>
+      </EditFolder>
+      <DeleteFolderModal
+        isModalOpen={isDeleteModalOpen}
+        closeModal={closeDeleteModal}
+        folderName={selectedFolderInfo?.name}
+        folderId={selectedFolderInfo?.id}
+        setFolderList={setFolderList}
+      />
+    </FolderControlsContainer>
+  );
+};
+
+export default FolderControls;
---
+++ myapp/src/components/Links/FolderControls/FolderControlsStyle.ts
@@ -0,0 +1,24 @@
+import styled from 'styled-components';
+
+export const FolderControlsContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 1rem;
+`;
+
+export const EditFolder = styled.button`
+  font-size: 1.4rem;
+  font-weight: 600;
+  line-height: 1.6rem;
+  background-color: transparent;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  color: var(--gray40);
+`;
+
+export const FolderControlsImg = styled.img`
+  width: 1.8rem;
+  height: 1.8rem;
+`;
