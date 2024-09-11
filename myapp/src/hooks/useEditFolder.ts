147940cae1ccc92a0af99e5bce7c56df04495e6a
seunghyun <tmdgus7048@naver.com>
2024-09-12 03:19:31 +0900

    feat : updateFolder함수호출 커스텀훅 추가

---
+++ myapp/src/hooks/useEditFolder.ts
@@ -0,0 +1,24 @@
+import { useState } from 'react';
+import { updateFolder } from '../apis/LinksPageApi/forderApi';
+
+const useEditFolder = (folderId: number | undefined, editValue: string) => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState<Error | null>(null);
+
+  const fetchData = async () => {
+    if (folderId) {
+      setIsLoading(true);
+      setError(null);
+      try {
+        const response = await updateFolder(folderId, editValue);
+        return response;
+      } catch (error) {
+        setError(error as Error);
+      } finally {
+        setIsLoading(false);
+      }
+    }
+  };
+  return { isLoading, fetchData };
+};
+export default useEditFolder;
