abd4d3a183f7391c87f78838c2e6598d458cbd8e
seunghyun <tmdgus7048@naver.com>
2024-09-11 12:40:38 +0900

    feat : LinksPage header컴포넌트 추가

--- myapp/src/pages/Links.tsx
+++ myapp/src/pages/Links.tsx
@@ -1,9 +1,11 @@
+import Header from '../components/@Shared/Header/Header';
 import AddLink from '../components/Links/AddLink/AddLink';
 import LinksWrapper from '../components/Links/LinksWrapper/LinksWrapper';
 
 function Links() {
   return (
     <>
+      <Header />
       <AddLink />
       <LinksWrapper />
     </>
