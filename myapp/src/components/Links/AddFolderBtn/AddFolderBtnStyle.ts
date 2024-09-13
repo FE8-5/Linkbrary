b126846ca529a62793091b2ec3b19086c3f3eae9
seunghyun <tmdgus7048@naver.com>
2024-09-13 18:18:40 +0900

    style : 모바일 화면일떄 폴더추가 버튼 스타일 변경

--- myapp/src/components/Links/AddFolderBtn/AddFolderBtnStyle.ts
+++ myapp/src/components/Links/AddFolderBtn/AddFolderBtnStyle.ts
@@ -1,17 +1,30 @@
 import styled from 'styled-components';
+import { BREAKPOINTS } from '../../../constatnts/Breakpoint';
 
 export const AddFolderButton = styled.button`
   font-size: 1.6rem;
   font-weight: 500;
   line-height: 1.9rem;
   color: var(--primary);
-  background-color: white;
+  background-color: var(--white);
+  border: 1px solid var(--primary);
+  padding: 1rem;
+  border-radius: 2rem;
   display: flex;
   align-items: center;
   justify-content: center;
   gap: 0.3rem;
   width: 11rem;
-  padding: 0 1rem;
+  transform: 'translate(-50%, -50%)';
+  position: fixed;
+  bottom: 5rem;
+  left: 35%;
+  @media screen and (min-width: ${BREAKPOINTS.tablet}) {
+    position: inherit;
+    border: none;
+    padding: 0 1rem;
+    transform: none;
+  }
 `;
 
 export const PlusIcon = styled.img`
